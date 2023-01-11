216b22869be73ca5ed311136132c831671362ec6
Anuj Kumar Karmakar <80219364+anujkkarmakar@users.noreply.github.com>
2023-01-11 20:43:17 +0530

    Slider Pane

--- app/src/main/java/com/example/fooddiz/HomeActivity.java
+++ app/src/main/java/com/example/fooddiz/HomeActivity.java
@@ -1,9 +1,13 @@
 package com.example.fooddiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -12,9 +16,28 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private AppCompatButton signoutbtn;
+    private FirebaseUser firebaseUser;
+    private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        signoutbtn = findViewById(R.id.signoubtn);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        //sign out button
+        signoutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                //update UI
+                Toast.makeText(HomeActivity.this, "Successfully sign out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(HomeActivity.this, ParentActivity.class));
+            }
+        });
     }
 }
